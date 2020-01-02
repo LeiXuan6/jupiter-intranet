f2b9f4f508789c0477eace8b522ccdf4f5482e31
leixuan <leixuan@maowan.com>
2020-01-02 17:38:51 +0800

    protobuf serializer 更新

--- jupiter-serialization/jupiter-serialization-protobuf/src/main/java/org/jupiter/serialization/protobuf/ProtobufSerializer.java
+++ jupiter-serialization/jupiter-serialization-protobuf/src/main/java/org/jupiter/serialization/protobuf/ProtobufSerializer.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (c) 2015 The Jupiter Project
+ *
+ * Licensed under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jupiter.serialization.protobuf;
 
-import com.google.protobuf.GeneratedMessageV3;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 import com.google.protobuf.Parser;
 import org.jupiter.common.util.internal.logging.InternalLogger;
 import org.jupiter.common.util.internal.logging.InternalLoggerFactory;
@@ -10,7 +25,6 @@ import org.jupiter.serialization.SerializerType;
 import org.jupiter.serialization.io.InputBuf;
 import org.jupiter.serialization.io.OutputBuf;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -33,7 +47,7 @@ public class ProtobufSerializer extends Serializer {
 
     @Override
     public <T> byte[] writeObject(T obj) {
-        return ((GeneratedMessageV3)obj).toByteArray();
+        return ((Message.Builder)obj).build().toByteArray();
     }
 
     @Override
